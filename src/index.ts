937ec797782bcc9b26c8109f5bba4f93fdadf769
BadAimWeeb <badaimweeb@protonmail.com>
2021-09-30 00:23:07 +0700

    scanning for possible modules

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,32 @@
 import fs from "node:fs/promises";
 import path from "node:path";
+import crypto from "node:crypto";
 import { ConfigInterface } from "./interface";
 import NCBModule from "./module";
 
 const defaultCfg = {};
 
 export default class NCBCore {
+    runInstanceID = "00000000000000000000000000000000";
+
     starting = false;
     running = false;
 
     profile_directory!: string;
     config: ConfigInterface = {};
-    module: NCBModule[] = [];
+    module: { [id: string]: NCBModule } = {};
+    unassignedModuleID = 1;
 
     constructor(profile_directory: string) {
-        this.profile_directory = profile_directory || process.cwd();
+        this.profile_directory = profile_directory || process.cwd();        
     }
 
     async start() {
         if (!this.starting && !this.running) {
             this.starting = true;
+            this.runInstanceID = crypto.randomBytes(16).toString("hex");
             await this.loadConfig();
+            await this.createTemp();
 
             await this.initializeModules();
             this.starting = false;
@@ -29,8 +35,9 @@ export default class NCBCore {
     }
 
     async stop() {
-        if (this.running) {
+        if (this.running && !this.starting) {
             await this.killModules();
+            await this.clearTemp();
 
             this.running = false;
         }
@@ -48,11 +55,42 @@ export default class NCBCore {
         }
     }
 
+    async scanModules() {
+        try {
+            let b = path.join(this.profile_directory, "modules");
+            let dir = await fs.readdir(b, { withFileTypes: true, encoding: "utf8" });
+            return dir.filter(f => f.isFile() && path.parse(f.name).ext === ".zip").map(f => path.join(b, f.name));
+        } catch {
+            return [];
+        }
+    }
+
     async initializeModules() {
-        
+        let mod = await this.scanModules();
+        for (let mDir of mod) {
+            this.module[(this.unassignedModuleID++).toString()] = new NCBModule(mDir);
+        }
     }
 
     async killModules() {
+        await Promise.all(Object.values(this.module).map(m => m.stop()));
+        for (let mID in this.module) {
+            delete this.module[mID];
+        }
+    }
 
+    async createTemp() {
+        try {
+            await fs.mkdir(path.join(this.profile_directory, "temp", this.runInstanceID), { recursive: true });
+        } catch (e) {
+            let ex = e as any as Error;
+            throw new Error("Cannot create temp folder. You should try restart first, then file a bug if the error presist.\n" + ex?.stack ?? e);
+        }
+    }
+
+    async clearTemp() {
+        try {
+            await fs.rm(path.join(this.profile_directory, "temp", this.runInstanceID), { recursive: true });
+        } catch {}
     }
 }
--- src/module.ts
+++ src/module.ts
@@ -1,3 +1,15 @@
 export default class NCBModule {
+    moduleDir: string;
 
+    constructor(moduleZIPDir: string) {
+        this.moduleDir = moduleZIPDir;
+    }
+
+    async start() {
+
+    }
+
+    async stop() {
+        
+    }
 }
